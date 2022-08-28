d5202eae9de405aadca1b6fae307d76280b74644
biswamantu2022 <biswanathnahak2012@gmail.com>
2022-08-28 11:15:47 +0530

    database connect to python

--- database/dbcon.py
+++ database/dbcon.py
@@ -1,1 +1,21 @@
-hello
+#hello
+
+import pandas as pd
+import numpy as np
+import pymysql
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='1234',
+                             database='biswadb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+cursor=connection.cursor()
+query='select * from employee'
+cursor.execute(query)
+cursor.fetchall()
+
+df_pd=pd.DataFrame(cursor.fetchall())
+df_pd
+connection.close()
+
