cdd1b7d6fb3acc44ea34640e1f35e80aace26c38
biswamantu2022 <biswanathnahak2012@gmail.com>
2022-12-03 19:36:24 +0530

    new

--- file1.py
+++ file1.py
@@ -4,3 +4,11 @@
 print("Hello world")
 name=input('enter your name:')
 print(name)
+
+contact=eval(input('enter your mobile number'))
+def print_info(contact):
+    assert contact is int,'enter only int'
+    print(f'your number is {contact} ')
+
+
+print_info(contact)
